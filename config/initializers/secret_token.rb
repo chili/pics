b9a0519916f5728b82b92433691e6bddcdb88805
yechanglun <yechanglun@gmail.com>
2011-11-25 22:45:00 +0800

    初始化信息采集项目

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pics::Application.config.secret_token = '95645ebab747628b33493530787139db9e4b396c331b2602fa3c20e8d17e48fea1e2211b1e140e9b0cf310c7224d4efd444f6b6bfb3f87b984fd610c489edc20'
